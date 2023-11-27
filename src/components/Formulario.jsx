d6c4de318910e4f616f87895fb9a6acefc301ec7
Jafet <jafet@Sauds-MBP.localdomain>
2023-11-27 15:29:34 -0800

    first commit

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,104 @@
+import { useEffect, useState } from 'react'
+import Error from './Error';
+
+const Formulario = ({ pacientes, setPacientes, paciente ,setPaciente}) => {
+    const [nombre, setNombre] = useState('');
+    const [propietario, setPropietario] = useState('');
+    const [email, setEmail] = useState('');
+    const [fecha, setFecha] = useState('');
+    const [sintomas, setSintomas] = useState('');
+
+    const [error, setError] = useState(false)
+
+    useEffect(() => {
+        if (Object.keys(paciente).length > 0) {
+            setNombre(paciente.nombre)
+            setPropietario(paciente.propietario)
+            setEmail(paciente.email)
+            setFecha(paciente.fecha)
+            setSintomas(paciente.sintomas)
+        }
+
+    }, [paciente])
+
+    const generarId = () => {
+        const random = Math.random().toString(36).substring(2);
+        const fecha = Date.now().toString(36)
+
+        return random + fecha
+
+    }
+    const handleSubmint = (e) => {
+        e.preventDefault()
+        if ([nombre, propietario, email, fecha, sintomas].includes('')) {
+            setError(true)
+            return;
+        }
+        setError(false);
+        //objeto paciente
+
+        const objetoPaciente = {
+            nombre,
+            propietario,
+            email,
+            fecha,
+            sintomas,
+            id: generarId()
+        }
+
+        if (paciente.id) {
+            //editando registro
+            objetoPaciente.id= paciente.id
+            const pacientesActualizados = pacientes.map(pacienteState => pacienteState.id === paciente.id ? objetoPaciente : pacienteState)
+            setPacientes(pacientesActualizados)
+            setPaciente({})
+        } else {
+            // nuevo registro
+            objetoPaciente.id= generarId();
+            setPacientes([...pacientes, objetoPaciente]);
+        }
+
+
+
+        //reinicio formulario
+        setNombre('')
+        setPropietario('')
+        setEmail('')
+        setFecha('')
+        setSintomas('')
+
+    }
+
+    return (
+        <div className="md:w-1/2 lg:w-2/5 mx-5">
+            <h2 className='font-black text-3xl text-center'>Seguimiento Pacientes</h2>
+            <p className='text-lg mt-5 text-center mb-10'>Añade Pacientes y {' '} <span className='text-indigo-600 font-bold'>Administralos</span></p>
+            <form onSubmit={handleSubmint} action="" className='bg-white shadow-md rounded-lg py-10 px-5 mb-10'>
+                {error && <Error><p>Todos los campos son requeridos</p></Error>}
+                <div className='mb-5'>
+                    <label htmlFor="mascota" className='block text-gray-700 uppercase font-bold'>Nombre mascota</label>
+                    <input onChange={(e) => setNombre(e.target.value)} value={nombre} id="mascota" type="text" placeholder='Nombre de la mascota' className='border-2 w-full p-2 m-2 placeholder-gray-400 rounded-md' />
+                </div>
+                <div className='mb-5'>
+                    <label htmlFor="propietario" className='block text-gray-700 uppercase font-bold'>Nombre propietario</label>
+                    <input onChange={(e) => setPropietario(e.target.value)} value={propietario} id="propietario" type="text" placeholder='Nombre del propietario' className='border-2 w-full p-2 m-2 placeholder-gray-400 rounded-md' />
+                </div>
+                <div className='mb-5'>
+                    <label htmlFor="email" className='block text-gray-700 uppercase font-bold'>Email</label>
+                    <input onChange={(e) => setEmail(e.target.value)} value={email} id="email" type="email" placeholder='Email contacto Propietario' className='border-2 w-full p-2 m-2 placeholder-gray-400 rounded-md' />
+                </div>
+                <div className='mb-5'>
+                    <label htmlFor="alta" className='block text-gray-700 uppercase font-bold'>Alta</label>
+                    <input onChange={(e) => setFecha(e.target.value)} value={fecha} id="alta" type="date" className='border-2 w-full p-2 m-2 placeholder-gray-400 rounded-md' />
+                </div>
+                <div className='mb-5'>
+                    <label htmlFor="sintomas" className='block text-gray-700 uppercase font-bold'>Sintomas</label>
+                    <textarea onChange={(e) => setSintomas(e.target.value)} value={sintomas} placeholder='Describe los sintomas' id="sintomas" cols="30" rows="10"></textarea>                </div>
+
+                <input value={paciente.id ? 'Editar Paciente' : 'Agregar Paciente'} type="submit" className='bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-all' />
+            </form>
+        </div>
+    )
+}
+
+export default Formulario
